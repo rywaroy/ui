309286b5594a8cdc9dfd5536f285a940866c9b5b
zhangzhihao <464485682@qq.com>
2018-05-08 17:59:50 +0800

    button 组件

--- src/packages/Button/src/Button.vue
+++ src/packages/Button/src/Button.vue
@@ -1,8 +1,39 @@
 <template>
-  <div class="yg-button">111</div>
+  <button class="yg-button"
+    @click="handleClick"
+    :class="[
+      `yg-button--${type}`,
+      `yg-button--${size}`,
+      {
+        'is-disabled': disabled,
+      }
+    ]"
+    :disabled="disabled"
+  >
+    <slot></slot>
+  </button>
 </template>
 <script>
 export default {
-  name: 'yg-button',
+  name: 'YgButton',
+  props: {
+    type: {
+      type: String,
+      default: 'default',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: 'medium',
+    },
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt);
+    },
+  },
 };
 </script>
--- src/pages/Button.vue
+++ src/pages/Button.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
-    <yg-button></yg-button>
+    <yg-button size="medium">按钮</yg-button>
+    <yg-button size="small">按钮</yg-button>
+    <yg-button size="mini">按钮</yg-button>
+    <yg-button size="mini" disabled>按钮</yg-button>
+    <br>
+    <br>
+    <yg-button size="medium" type="primary">按钮</yg-button>
+    <yg-button size="small" type="primary">按钮</yg-button>
+    <yg-button size="mini" type="primary">按钮</yg-button>
+    <yg-button size="mini" type="primary" disabled>按钮</yg-button>
+    <br>
+    <br>
+    <yg-button size="medium" type="secondary">按钮</yg-button>
+    <yg-button size="small" type="secondary">按钮</yg-button>
+    <yg-button size="mini" type="secondary">按钮</yg-button>
+    <yg-button size="mini" type="secondary" disabled>按钮</yg-button>
+    <br>
+    <br>
+    <yg-button size="medium" type="normal">按钮</yg-button>
+    <yg-button size="small" type="normal">按钮</yg-button>
+    <yg-button size="mini" type="normal">按钮</yg-button>
+    <yg-button size="mini" type="normal" disabled>按钮</yg-button>
+    <br>
+    <br>
+    <yg-button size="medium" type="warning">按钮</yg-button>
+    <yg-button size="small" type="warning">按钮</yg-button>
+    <yg-button size="mini" type="warning">按钮</yg-button>
+    <yg-button size="mini" type="warning" disabled>按钮</yg-button>
   </div>
 </template>
